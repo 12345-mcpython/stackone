634673ffb364f3011a615a4d14726d0a9f636fb5
m1504 <m15043340061@163.com>
2022-04-18 21:46:07 +0800

    Use new get item way and add logo.png.

--- src/main/java/com/laosun/stackone/StackOneMod.java
+++ src/main/java/com/laosun/stackone/StackOneMod.java
@@ -5,6 +5,7 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.ForgeRegistries;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -22,14 +23,9 @@ public class StackOneMod {
 
     // Use the java's reflect to edit the max stack size.
     private void setup(final FMLCommonSetupEvent event) {
-        for (int i = 1; ; i++) {
+        for (Item i : ForgeRegistries.ITEMS) {
             try {
-                Item item = Item.byId(i);
-                Item air = Item.byId(0);
-                if (item.equals(air)) {
-                    break;
-                }
-                LOGGER.debug(item.getRegistryName());
+                LOGGER.debug(i.getRegistryName());
                 Class<? extends Item> a = Item.class;
                 Field f2;
                 try {
@@ -38,7 +34,7 @@ public class StackOneMod {
                     f2 = a.getDeclaredField("field_77777_bU");
                 }
                 f2.setAccessible(true);
-                f2.set(item, 1);
+                f2.set(i, 1);
             } catch (Exception e) {
                 e.printStackTrace();
             }
