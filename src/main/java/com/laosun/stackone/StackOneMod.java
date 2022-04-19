c0c697aedbef18bcec35bd773cc9f3f44ba79cca
m1504 <m15043340061@163.com>
2022-04-19 19:56:57 +0800

    Run it with product environment.

--- src/main/java/com/laosun/stackone/StackOneMod.java
+++ src/main/java/com/laosun/stackone/StackOneMod.java
@@ -3,22 +3,23 @@ package com.laosun.stackone;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.item.Item;
 import net.minecraft.util.registry.Registry;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+
 
 import java.lang.reflect.Field;
-
+import java.util.logging.Logger;
+import java.util.logging.Level;
 public class StackOneMod implements ModInitializer {
     // This logger is used to write text to the console and the log file.
     // It is considered best practice to use your mod id as the logger's name.
     // That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("stackone");
+    public static final org.apache.logging.log4j.Logger LOGGER = LogManager.getLogger();
 
     @Override
     public void onInitialize() {
         for (Item i : Registry.ITEM) {
             try {
-                LOGGER.debug(i.getTranslationKey());
+                LOGGER.info(i.getTranslationKey());
                 Class<? extends Item> a = Item.class;
                 Field f2;
                 try {
