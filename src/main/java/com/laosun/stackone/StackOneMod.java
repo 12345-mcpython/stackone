c4dd366c15870ea736cda3c2d1a228f5d32134a2
m1504 <m15043340061@163.com>
2022-04-20 08:57:29 +0800

    Start 1.14.4 forge project

---
+++ src/main/java/com/laosun/stackone/StackOneMod.java
@@ -0,0 +1,45 @@
+package com.laosun.stackone;
+
+import net.minecraft.item.Item;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.ForgeRegistries;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.lang.reflect.Field;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("stackone")
+public class StackOneMod {
+    // Directly reference a log4j logger.
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public StackOneMod() {
+        // Register the setup method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        MinecraftForge.EVENT_BUS.register(this);
+        // field_77777_bU
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+        try {
+            for (Item i : ForgeRegistries.ITEMS) {
+                Field field;
+                try {
+                    field = Item.class.getDeclaredField("maxStackSize");
+                } catch (NoSuchFieldException e) {
+                    field = Item.class.getDeclaredField("field_77777_bU");
+                }
+                field.setAccessible(true);
+                field.setInt(i, 1);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
