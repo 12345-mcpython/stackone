268c5c42460118e57cc0bbffce28686546ddfe6f
m1504 <m15043340061@163.com>
2022-05-01 17:24:18 +0800

    Change version to 1.0.1

--- src/main/java/com/laosun/stackone/StackOneMod.java
+++ src/main/java/com/laosun/stackone/StackOneMod.java
@@ -12,7 +12,7 @@ import org.apache.logging.log4j.Logger;
 public class StackOneMod {
     public static final String MODID = "stackone";
     public static final String NAME = "Stack One Mod";
-    public static final String VERSION = "1.12.2-1.0.0";
+    public static final String VERSION = "1.12.2-1.0.1";
 
     public static Logger logger;
 
